531286437061627b76c8410ffacb8a26d29ac694
Hirokazu Hirono <lost.my.phone.8.5@gmail.com>
2018-03-04 13:26:50 +0900

    created year property

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,7 +1,53 @@
 const zh = require('./../');
 
-console.log(zh.year);
-
 test('should return this year.', () => {
-  expect(zh.year).toBe(new Date().getFullYear());
+  expect(zh.year).toBe(outputZATime().getUTCFullYear());
 });
+
+test('should have 2 hours difference between UTC and SAST.', () => {
+  expect(outputZATime().getUTCHours() - new Date().getUTCHours()).toBe(2);
+});
+
+function outputZATime() {
+  const southAfricanTime = new Date(
+    Date.UTC(
+      new Date().getUTCFullYear(),
+      new Date().getUTCMonth(),
+      new Date().getUTCDate(),
+      new Date().getUTCHours() + 2,
+      new Date().getUTCMinutes(),
+      new Date().getUTCSeconds(),
+      new Date().getUTCMilliseconds()
+    )
+  );
+  // output: Date => UTC + 2
+  return southAfricanTime;
+  // return parseInt(southAfricanTime.toUTCString().slice(12, 16), 10);
+}
+
+// const southAfricanTime = new Date(
+//   Date.UTC(
+//     new Date().getFullYear(),
+//     new Date().getUTCMonth(),
+//     new Date().getUTCDate(),
+//     new Date().getUTCHours() + 2,
+//     new Date().getUTCMinutes(),
+//     new Date().getUTCSeconds(),
+//     new Date().getUTCMilliseconds()
+//   )
+// );
+
+// function outputDay() {
+//   const southAfricanTime = new Date(
+//     Date.UTC(
+//       new Date().getUTCFullYear(),
+//       new Date().getUTCMonth(),
+//       new Date().getUTCDate(),
+//       new Date().getUTCHours() + 2,
+//       new Date().getUTCMinutes(),
+//       new Date().getUTCSeconds(),
+//       new Date().getUTCMilliseconds()
+//     )
+//   );
+//   return southAfricanTime.toUTCString().slice(0, 3); // output: string => Sun, Mon
+// }
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 (function(root, undefined) {
   const libs = {};
 
-  libs.year = new Date().getFullYear();
+  libs.year = new Date(
+    Date.UTC(
+      new Date().getFullYear(),
+      new Date().getUTCMonth(),
+      new Date().getUTCDate(),
+      new Date().getUTCHours() + 2,
+      new Date().getUTCMinutes(),
+      new Date().getUTCSeconds(),
+      new Date().getUTCMilliseconds()
+    )
+  ).getUTCFullYear();
 
   libs.holidays = [
     {
-      Jan: [{ day: 1, name: "New Year's Day" }]
+      Jan: [{ date: 1, name: "New Year's Day" }]
     }
   ];
 
